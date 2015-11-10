0b6fbb5ba825ba0347657c2360a5e8a5f5c00e81
dlee0113 <dlee0113@gmail.com>
2015-11-10 13:50:50 -0500

    added array as a parameter in search method

--- interviewcake/src/main/java/find_in_ordered_set/App.java
+++ interviewcake/src/main/java/find_in_ordered_set/App.java
@@ -3,13 +3,7 @@ package find_in_ordered_set;
 // https://www.interviewcake.com/question/find-in-ordered-set
 // http://algorithms.tutorialhorizon.com/binary-search/
 public class App {
-	private int[] arrA;
-
-	public App(int[] arrA) {
-		this.arrA = arrA;
-	}
-
-	public Boolean Search(int low, int high, int number) {
+	public Boolean Search(int low, int high, int number, int[] arrA) {
 		if (low > high) {
 			return false;
 		}
@@ -18,17 +12,18 @@ public class App {
 		if (arrA[mid] == number)
 			return true;
 		else if (arrA[mid] > number)
-			return Search(low, mid - 1, number);
+			return Search(low, mid - 1, number, arrA);
 		else
-			return Search(mid + 1, high, number);
+			return Search(mid + 1, high, number, arrA);
 	}
 
 	public static void main(String args[]) {
 		int[] a = { 2, 5, 8, 10, 14, 44, 77, 78, 99 };
 		int number = 99;
-		App b = new App(a);
-		System.out.println("The " + number + " present in array a ??? :"+ b.Search(0, a.length - 1, number));
+		
+		App b = new App();
+		System.out.println("The " + number + " present in array a ??? :"+ b.Search(0, a.length - 1, number, a));
 		number = 76;
-		System.out.println("The " + number + " present in array a ??? :"+ b.Search(0, a.length - 1, number));
+		System.out.println("The " + number + " present in array a ??? :"+ b.Search(0, a.length - 1, number, a));
 	}
 }
